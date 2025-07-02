b63c9372e164342111586ae4a2615ce62e97b7a7
Amaan Mirza <amaanmirza287@gmail.com>
2025-07-02 19:09:41 +0530

    made the dashboard dynamic

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,9 +1,79 @@
+"use client";
 import DashboardContent from "@/components/Dashboard/DashboardContent";
+import { UserState } from "@/types/userstate";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
+interface Course {
+  course: {
+    id: string;
+    title: string;
+    _count: { classes: number }; //number of classes in a course
+  };
+}
+
+interface Classes {
+  id: string;
+  title: string;
+  course: {
+    title: string;
+  }; // course which this class belongs to
+  createdAt: string;
+  isLive: boolean;
+  isRecorded: boolean;
+  scheduledAt: string;
+}
+interface SelectedCourse {
+  title: string;
+  thumbnail: string;
+}
 export default function Dashboard() {
+  const [classes, setClasses] = useState<Classes[]>([]);
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [selectedCourse, setSelectedCourse] = useState<SelectedCourse>();
+  const user = useSelector((state: { user: UserState }) => state.user);
+
+  useEffect(() => {
+    //to get the courses data
+    async function getEnrolledCourses() {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}course-enrollment/courses/${user.user?.id}`
+      );
+      setCourses(response.data);
+    }
+    //to get the classes in enrolled courses data
+    async function getClassesInCourses() {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}class/all/${user.selectedCourse?.courseId}`,
+        {
+          headers: { Authorization: `Bearer ${user.accessToken}` },
+        }
+      );
+
+      console.log(response.data);
+      setClasses(response.data);
+    }
+    //to get the currrently selected course data
+    async function getCourseById() {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}course/getCourseById/${user.selectedCourse?.courseId}`
+      );
+      setSelectedCourse(response.data);
+    }
+    getEnrolledCourses();
+    getCourseById();
+    getClassesInCourses();
+  }, []);
+
   return (
     <div>
-      <DashboardContent />
+      <DashboardContent
+        user={{ name: user.user?.username ?? "" }}
+        classes={classes}
+        courses={courses}
+        selectedCourse={selectedCourse ?? { title: "", thumbnail: "" }}
+      />
     </div>
   );
 }
---
+++ src/types/DataTypes.ts
@@ -0,0 +1,101 @@
+export interface User {
+  id: string;
+  profileImage: string;
+  email: string;
+  phoneNumber?: string;
+  isVerified: boolean;
+  username: string;
+  name: string;
+  role: Role;
+  createdCourses: Course[];
+  enrolledCourses: CourseEnrollment[];
+  createdClasses: Classes[];
+  createdAt: string;
+  updatedAt: string;
+}
+enum Role {
+  STUDENT = "STUDENT",
+  TEACHER = "TEACHER",
+  ADMIN = "ADMIN",
+  SUPER_ADMIN = "SUPER_ADMIN",
+}
+export interface CourseEnrollment {
+  student: User;
+  studentId: string;
+  course: Course;
+  courseId: string;
+  enrolledAt: string; //Date and time
+  completedAt: string; //Date and time
+  lastAccessedAt: string; //Date and time
+}
+export interface Course {
+  id: string;
+  title: string;
+  description: string;
+  thumbnail?: string;
+  isActive: boolean;
+  price?: number;
+  category?: string;
+  creator: User;
+  creatorId: string;
+  enrollments: CourseEnrollment[];
+  classes: Classes[];
+
+  createdAt: string;
+  updatedAt: string;
+  Note: Note[];
+  Assignment: Assignment[];
+  Attachments: Attachment[];
+}
+export interface Note {
+  id: string;
+  notesHtml: string;
+  class: Classes;
+  classId: string;
+  course: Course;
+  courseId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+export interface Attachment {
+  id: string;
+  attachment: string;
+  class: Classes;
+  classId: string;
+  course: Course;
+  courseId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+export interface Assignment {
+  id: string;
+  assignments: string;
+  class: Classes;
+  classId: string;
+  course: Course;
+  courseId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Classes {
+  id: string;
+  title: string;
+  description: string;
+  videoLink?: string;
+  zoomLink?: string;
+  attachments: Attachment[];
+  notes: Note[];
+  assignments: Assignment[];
+  course: Course;
+  courseId: string;
+  creator: User;
+  creatorId: string;
+  scheduledAt?: string; //date and time
+  attendanceCount: number;
+  isLive: boolean;
+  isRecorded: boolean;
+  isActive: boolean;
+  createdAt: string; //date and time
+  updatedAt: string; //date and time
+}
